cef6cab56e2e44a957434ec48e73b3f1b890cf3b
Ken-HH24 <1098862703@qq.com>
2021-12-17 11:09:18 +0800

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,6 @@
 import '../styles/globals.css'
 import { QueryClient, QueryClientProvider } from 'react-query'
-import axios from 'axios';
 import { CookiesProvider } from 'react-cookie';
-import { parseCookies } from '../helpers';
 
 const queryClient = new QueryClient();
 
